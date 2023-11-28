1fa7af6e92966ab503d6ef4055e3aa08045fb62c
Myrna <myrnapt96@gmail.com>
2023-11-29 00:48:41 +0100

    useNewUrlParser true

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: "./variables.env" });
 
 const conectarDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {});
+    await mongoose.connect(process.env.MONGODB_URI, () => { }, { useNewUrlParser: true });
     console.log("Database connected");
   } catch (error) {
     console.log(error);
