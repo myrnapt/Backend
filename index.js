af0ec0480dad3d7200d23c4488e2fd46f4a75bcb
Myrna <myrnapt96@gmail.com>
2023-12-01 16:20:19 +0100

    cambip app.use cors

--- index.js
+++ index.js
@@ -18,13 +18,26 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-app.use(cors({
-  allowedHeaders: '*',
-  origin: '*',
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-}));
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://your-frontend.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
 
+  next();
+});
 // Routes
 app.get('/', (req, res) => {
   res.send({ title: 'Eventos' });
